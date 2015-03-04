b6fef619864f9cae5d730bbd0d7dfb55abe45279
oarques <oarques@gmail.com>
2015-03-04 17:04:33 +0100

    Estructura cartas

--- Poker.php
+++ Poker.php
@@ -1,11 +1,7 @@
 <?php include "_header.php" ?>
 
 <div class="well text-center"><h1>Poker<h1></div>
-		<div class="row">
-			
-			<div class="col-xs-6 col-xs-offset-4" id="tablas3" ></div>
-				<div class="well"id="tablas1"></div>	
-		</div>
+		
 
 
  <div class="row  text-center"><h2>Cartas<h2></div>
@@ -13,32 +9,32 @@
      
      <div class="row">
      		
-		<div class="thumbnail col-xs-6 col-xs-offset-3">
-					       <?php 
-					        if (isset($_POST["parar_A"])){
-					        echo $imagen1;
-					        echo $imagen2;
-					        echo "<b>Resultado = " . ($d1 + $d2) . "</b>";
-					        
-					        }
-					        if (isset($_POST["girar_A"])){
-					        echo $dado;
-					        }
-					         if (isset($_POST["parar_B"])){
-					        echo $dado;
-					        }
-					        if (isset($_POST["girar_B"])){
-					        echo $dado;
-					        }
-	
-
-					        ?>
+			<div class="row">	
+			<div class="col-xs-2 col-xs-offset-5" id="tablas3"><h4>Resultado<h4>	
+				<div class="well" id="tablas4">
+				
+				</div>
 			</div>
-					        	
-					        	<input name="girar_A" type="submit" value="Play" class="bnt btn-success" role="button" />
-					        	<input name="parar_A" type="submit" value="Stop" class="bnt btn-primary" role="button" />
-
+		</div>
 
 	
+
+
+		<form action="Poker.php" method="post">
+
+		<div class="text-center">	
+				
+
+				<input type="submit" name="dar" value="Dar" id="tablas3">
+
+		</div>
+
+
+
+
+
+
+		</form>
+
 
 <?php include "_footer.php" ?>
