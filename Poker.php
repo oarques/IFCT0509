959c471672f3190a01139d0e2d7a20cca33a36de
oarques <oarques@gmail.com>
2015-03-04 20:45:23 +0100

    resultado de poker sin cuadrar

--- Poker.php
+++ Poker.php
@@ -1,18 +1,48 @@
+<?php 
+if(isset($_POST["varagar"])){
+$imagen1= "img/dorso.jpg" ; 
+$imagen2= "img/dorso.jpg" ;
+}
+
+if(isset($_POST["dar"])){
+
+$c1=rand(1,13);
+$p1=rand(1,4);
+
+$c2=rand(1,13);
+$p2=rand(1,4);
+
+
+$imagen1= "img/" . $c1 . "_" . $p1 . ".jpg" ;
+$imagen2= "img/" . $c2 . "_" . $p2 . ".jpg" ;
+
+
+$cartas = ["As","2","3","4","5","6","7","8","9","10","J","Q","K"];
+$palos = ["picas","treboles","diamantes","corazones"];
+}
+
+ ?>
 <?php include "_header.php" ?>
 
 <div class="well text-center"><h1>Poker<h1></div>
 		
 
 
- <div class="row  text-center"><h2>Cartas<h2></div>
+ <div class="text-center"><h2>Cartas<h2></div>
    	<form action="Poker.php" method="post">
      
-     <div class="row">
+     
      		
-			<div class="row">	
-			<div class="col-xs-2 col-xs-offset-5" id="tablas3"><h4>Resultado<h4>	
+		<div class="row">	
+			<div class="col-xs-4 col-xs-offset-4" id="tablas3"><h4>Resultado<h4>	
 				<div class="well" id="tablas4">
-				
+				<h5><?php echo $cartas[$c1-1] . " de " . $palos[$p1-1]; ?></h5>
+				 <img src="<?php echo $imagen1; ?> " >
+				<h5><?php echo $cartas[$c2-1] . " de " . $palos[$p2-1]; ?></h5>
+				 <img src="<?php echo $imagen2; ?> " >
+				 	
+                          
+					  
 				</div>
 			</div>
 		</div>
@@ -25,7 +55,8 @@
 		<div class="text-center">	
 				
 
-				<input type="submit" name="dar" value="Dar" id="tablas3">
+				<input type="submit" name="dar" value="Dar" id="tablas3"> 
+				<input type="submit" name="varagar" value="varagar" id="tablas3">
 
 		</div>
 
